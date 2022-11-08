7f454a64817206cf54e7fe5a4e0936b752bb244f
devnote-dev <inferno0745@gmail.com>
2022-11-08 17:12:12 +0000

    feat: implement config & basic panel interaction

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,10 @@ package cmd
 import (
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/go-playground/validator/v10"
+	"github.com/pteropackages/iris/config"
+	"github.com/pteropackages/iris/panel"
 	"github.com/pteropackages/iris/router"
 	"github.com/spf13/cobra"
 )
@@ -10,6 +14,26 @@ import (
 var rootCmd = &cobra.Command{
 	Use: "iris",
 	Run: func(cmd *cobra.Command, args []string) {
+		cfg, err := config.Get()
+		if err != nil {
+			if errs, ok := err.(validator.ValidationErrors); ok {
+				log.Error("failed to validate configuration")
+				for i, e := range errs {
+					log.WithFields(log.Fields{
+						"key":     e.Param(),
+						"message": e.Error(),
+					}).Errorf("error #%d", i)
+				}
+
+				return
+			} else {
+				log.WithField("error", err.Error()).Fatal("unexpected error when validating configuration")
+			}
+		}
+
+		client := panel.New(cfg)
+		if err = client.GetNodeInformation()
+
 		server := &http.Server{
 			Addr:    "localhost:5500",
 			Handler: router.Setup(),
---
+++ config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	_ "github.com/creasty/defaults"
+	"github.com/go-playground/validator/v10"
+	"gopkg.in/yaml.v3"
+)
+
+type NodeInformation struct {
+	ID    int
+	URL   string `json:"remote"`
+	Token string `json:"token"`
+}
+
+type Config struct {
+	Address string `default:"127.0.0.1" yaml:"address"`
+	Port    int64  `default:"5500" yaml:"port"`
+	Panel   struct {
+		URL string `yaml:"url"`
+		Key string `yaml:"key"`
+	} `yaml:"panel"`
+	Nodes map[int]*NodeInformation `default:"{}" yaml:"-"`
+}
+
+func Get() (*Config, error) {
+	cfg, err := GetStatic()
+	if err != nil {
+		return nil, err
+	}
+
+	validate := validator.New()
+	if err = validate.Struct(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func GetStatic() (*Config, error) {
+	root, _ := os.Getwd()
+	path := filepath.Join(root, "config.yml")
+	if _, err := os.Stat(path); err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg *Config
+	if err = yaml.Unmarshal(buf, &cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
