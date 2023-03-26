e21fc6e2fab9a650c2935972bded9d27efa2c497
devnote-dev <devnote.dev75@gmail.com>
2023-03-26 23:53:58 +0100

    feat: implement configuration

--- src/config.cr
+++ src/config.cr
@@ -4,5 +4,20 @@ module Iris
 
     property url : String
     property key : String # TODO: replace with custom JWT impl
+    property data : String
+    property servers : Array(String)
+
+    def self.load : Config
+      path = case
+             when File.exists? "iris.yml"
+               "iris.yml"
+             when File.exists? "iris.yaml"
+               "iris.yaml"
+             else
+               raise "cannot find config file"
+             end
+
+      from_yaml File.read path
+    end
   end
 end
