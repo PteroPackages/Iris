37d8067d628fe1cc6af4c905f04fc7618a239f86
devnote-dev <devnote.dev75@gmail.com>
2023-06-30 02:24:48 +0100

    refactor(config): reimplement config

--- src/config.cr
+++ src/config.cr
@@ -2,23 +2,41 @@ module Iris
   struct Config
     include YAML::Serializable
 
-    getter url : String
-    getter key : String # TODO: replace with custom JWT impl
-    getter? debug : Bool
-    getter data : String
+    DATA_HOME = begin
+      {% if flag?(:win32) %}
+        Path[ENV["APPDATA"]] / "iris"
+      {% else %}
+        if data = ENV["XDG_DATA_HOME"]?
+          Path[data] / "iris"
+        else
+          Path.home / ".local" / "share" / "iris"
+        end
+      {% end %}
+    end
+
+    getter panel_url : String
+    getter panel_key : String
     getter servers : Array(String)
+    getter extensions : Array(String)
 
     def self.load : Config
-      path = case
-             when File.exists? "iris.yml"
-               "iris.yml"
-             when File.exists? "iris.yaml"
-               "iris.yaml"
-             else
-               raise "cannot find config file"
-             end
+      from_yaml File.read(DATA_HOME / "config.yml")
+    end
 
-      from_yaml File.read path
+    def self.load_and_check : Config
+      unless Dir.exists? DATA_HOME / "extensions"
+        Dir.mkdir_p DATA_HOME / "extensions"
+      end
+
+      cfg = load
+      _ = URI.parse cfg.panel_url
+
+      # TODO: maybe replace with error enums design
+      raise "invalid panel api key" unless cfg.panel_key.starts_with? "ptlc_"
+      raise "no servers specified" if cfg.servers.empty?
+
+      # TODO: check and laod extensions
+      cfg
     end
   end
 end
